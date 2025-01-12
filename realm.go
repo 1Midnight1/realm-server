ec85b66c4277e8b98711d4bc1f57b8e2d0602a33
Midnight <abdulzulfiqar10@gmail.com>
2025-01-12 03:00:54 -0500

    docker set up

--- realm.go
+++ realm.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
 
-    "github.com/pocketbase/pocketbase"
-    "github.com/pocketbase/pocketbase/apis"
-    "github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
-    app := pocketbase.New()
+	app := pocketbase.New()
 
-    app.OnServe().BindFunc(func(se *core.ServeEvent) error {
-        // serves static files from the provided public dir (if exists)
-        se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
+	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		// serves static files from the provided public dir (if exists)
+		se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
 
-        return se.Next()
-    })
+		return se.Next()
+	})
 
-    if err := app.Start(); err != nil {
-        log.Fatal(err)
-    }
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
